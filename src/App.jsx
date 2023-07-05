d6b50cdeb87d539e89d14bfd27de37d11b8a6094
kore4677 <danielkore1234@gmail.com>
2023-07-05 16:30:52 +0100

    changed style

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,12 @@ import HomePage from "./components/HomePage";
 import AboutUs from "./components/AboutUs";
 import Contact from "./components/Contact/Contact";
 import Team from "./components/Team";
-// // import { Charts } from "./components/Charts/Charts";
+import { Charts } from "./components/Charts/Charts";
 import { AnimatePresence } from "framer-motion";
-import Method from "./components/Method";
 
 function App() {
+  
+
   return (
     <>
       <Router>
@@ -25,7 +26,7 @@ function App() {
           <Route path="/about" element={<AboutUs />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/team" element={<Team />} />
-          {/* <Route path="/port" element={<Charts />} /> */}
+          <Route path="/port" element={<Charts />} />
         </Routes>
       </Router>
     </>
