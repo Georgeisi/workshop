77d34efd98165a4d82ada2d65e77c63c9b212369
oketaiwoesther <oketaiwoesther@gmail.com>
2023-07-05 16:10:03 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,7 @@ import HomePage from "./components/HomePage";
 import AboutUs from "./components/AboutUs";
 import Contact from "./components/Contact/Contact";
 import Team from "./components/Team";
-<<<<<<< HEAD
-
-
-=======
 // import { Charts } from "./components/Charts/Charts";
->>>>>>> a13c0fa54b9bc613b334c19093204fe16d55e80f
 import { AnimatePresence } from "framer-motion";
 import Method from "./components/Method";
 
@@ -30,18 +25,9 @@ function App() {
           <Route path="/about" element={<AboutUs />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/team" element={<Team />} />
-<<<<<<< HEAD
           <Route path="/port" element={<Charts/>} />
           {/* <Route path="/our-method" element={<Our-method/>} /> */}
-=======
-<<<<<<< HEAD
-          {/* <Route path="/port" element={<Charts />} />
-           */}
-          <Route path="/our-method" element={<Method />} />
-=======
           {/* <Route path="/port" element={<Charts />} /> */}
->>>>>>> f7b527f8c8e67337e279ce8b5f38e76598dea9d6
->>>>>>> a13c0fa54b9bc613b334c19093204fe16d55e80f
         </Routes>
       </Router>
     </>
