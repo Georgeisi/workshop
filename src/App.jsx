4f434f4ed4a4933d1103cfc6f8257957b2a24809
Kingsley Solomon <kinxly@gmail.com>
2023-07-05 14:51:49 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,11 @@ import HomePage from "./components/HomePage";
 import AboutUs from "./components/AboutUs";
 import Contact from "./components/Contact/Contact";
 import Team from "./components/Team";
-import { Charts } from "./components/Charts/Charts";
+// import { Charts } from "./components/Charts/Charts";
 import { AnimatePresence } from "framer-motion";
+import Method from "./components/Method";
 
 function App() {
-  
-
   return (
     <>
       <Router>
@@ -26,7 +25,9 @@ function App() {
           <Route path="/about" element={<AboutUs />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/team" element={<Team />} />
-          <Route path="/port" element={<Charts />} />
+          {/* <Route path="/port" element={<Charts />} />
+           */}
+          <Route path="/our-method" element={<Method />} />
         </Routes>
       </Router>
     </>
